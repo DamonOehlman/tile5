26bf8022fcb1eba0c4a6e0bf042e379919fdd083
Damon Oehlman <damon.oehlman@gmail.com>
2010-06-18 12:58:32 +1000

    Added plugin section for extension to cross platform mobile frameworks

---
+++ src/js/plugins/phonegap/core.js
@@ -0,0 +1,35 @@
+SLICK.PhoneGap = (function() {
+    
+    function createTabItem(action) {
+        window.uicontrols.createTabBarItem(action.id, action.getParam("title"), action.getParam("icon"), {
+          onSelect: function() {
+              SLICK.Dispatcher.execute(action.id);
+          }
+        });
+    } // createTabItem
+    
+    // initialise module
+    var module = {
+        makeActionBar: function(actions) {
+            // if the actions aren't specified, get the registered actions from the dispatcher
+            if (! actions) {
+                actions = SLICK.Dispatcher.getRegisteredActions();
+            } // if
+            
+            window.uicontrols.createTabBar();
+            
+            // get the application modes
+            var activeTabs = ["UIControls.showTabBarItems"];
+            for (var ii = 0; ii < actions.length; ii++) {
+                createTabItem(actions[ii]);
+                activeTabs.push(actions[ii].id);
+            } // for
+
+            // show the tab bar
+            window.uicontrols.showTabBar();
+            PhoneGap.exec.apply(this, activeTabs);            
+        }
+    };
+    
+    return module;
+})();
