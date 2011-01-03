06519181fa2ba936c209b357226cb05beffe518d
Damon Oehlman <damon.oehlman@gmail.com>
2011-01-03 16:15:15 +1000

    Transition jodoc build to shell script also, removed build.xml

---
+++ build-docs.sh
@@ -0,0 +1,10 @@
+JODOC_PATH=/development/projects/github/sidelab/joDoc
+JODOC_DEPS_PATH=/development/tools/perl
+
+perl $JODOC_PATH/joDoc \
+     --output dist/docs \
+     --markdown $JODOC_DEPS_PATH/Markdown.pl \
+     --smartypants $JODOC_DEPS_PATH/SmartyPants.pl \
+     --title "Tile5 API Documentation" \
+     --template docs/html/template.html \
+     README.mdown LICENSE.mdown docs/ src/js
