882b903a6b009d91989fdc2724d285259ac47bd2
Damon Oehlman <damon.oehlman@gmail.com>
2013-09-20 20:09:44 +1000

    Initial commit of the new clean branch

---
+++ index.js
@@ -0,0 +1,12 @@
+/* jshint node: true */
+'use strict';
+
+/**
+  # Tile5
+
+  It's time for another crack at a pure
+  [canvas 2d context](http://www.w3.org/TR/2dcontext/) mapping library.
+
+  Let's see what magic we can pull off this time....
+
+**/
